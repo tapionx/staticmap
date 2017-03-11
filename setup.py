49775f4072491828271c379aa3fef12c0f9ffae4
tapionx <tapion@tapion.it>
2017-03-11 19:36:44 +0100

    Added GeodesicLinesToGIS to setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name='staticmap',
     packages=['staticmap'],
-    version='0.4.7',
+    version='0.4.8',
     description='A small, python-based library for creating map images with lines and markers.',
     author='Christoph Lingg',
     author_email='christoph@komoot.de',
-    url='https://github.com/komoot/staticmap',
-    download_url='https://github.com/komoot/staticmap/tarball/0.1',
+    url='https://github.com/tapionx/staticmap',
+    download_url='https://github.com/tapionx/staticmap/tarball/0.1',
     keywords='static map image osm',
     classifiers=[],
     install_requires=[
         'Pillow',
         'requests'
-
+        'GeodesicLinesToGIS'
     ]
 )
